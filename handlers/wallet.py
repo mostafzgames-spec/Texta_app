0258c3ac8cf566de9dadd37b01a4ade5bcbe1d33
mostafzgames-spec <mostafzgames@gmail.com>
2026-03-27 00:46:08 +0200

    Update wallet.py

--- handlers/wallet.py
+++ handlers/wallet.py
@@ -1,1 +1,9 @@
+from telegram import Update
+from telegram.ext import ContextTypes
+from database import get_balance
 
+async def wallet(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    user_id = update.effective_user.id
+    balance = get_balance(user_id)
+
+    await update.message.reply_text(f"💰 رصيدك: {balance}")
