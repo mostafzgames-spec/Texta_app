e93b86b033700c96f5c4a6bf47a85aad34820b0c
mostafzgames-spec <mostafzgames@gmail.com>
2026-03-27 00:45:43 +0200

    Update account.py

--- handlers/account.py
+++ handlers/account.py
@@ -1,1 +1,6 @@
+from telegram import Update
+from telegram.ext import ContextTypes
 
+async def account(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    user_id = update.effective_user.id
+    await update.message.reply_text(f"🆔 ID: {user_id}")
