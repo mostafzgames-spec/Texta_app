70c35067014903c494cb62b159b9db576abafe9d
mostafzgames-spec <mostafzgames@gmail.com>
2026-03-27 02:04:00 +0200

    Delete handlers/referral.py

--- handlers/referral.py
+++
@@ -1,8 +0,0 @@
-from telegram import Update
-from telegram.ext import ContextTypes
-
-async def referral(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    user_id = update.effective_user.id
-    link = f"https://t.me/Mafhumatk_bot?start={user_id}"
-
-    await update.message.reply_text(f"🔗 رابط الدعوة:\n{link}")
