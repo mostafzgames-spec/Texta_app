fc1758f6e0f1737e7affa999440d3d8f64660cea
mostafzgames-spec <mostafzgames@gmail.com>
2026-03-27 02:11:16 +0200

    Update database.py

--- database.py
+++ database.py
@@ -1,25 +1,19 @@
 import sqlite3
 import datetime
+import pytz
 
 conn = sqlite3.connect("bot.db", check_same_thread=False)
 cursor = conn.cursor()
 
-cursor.execute("""
-CREATE TABLE IF NOT EXISTS users (
-    id INTEGER PRIMARY KEY,
-    balance INTEGER DEFAULT 0,
-    last_daily INTEGER DEFAULT 0,
-    invited_by INTEGER,
-    join_date TEXT
-)
-""")
-conn.commit()
+# المنطقة الزمنية (مصر)
+egypt = pytz.timezone("Africa/Cairo")
 
 
 def add_user(user_id, invited_by=None):
     cursor.execute("SELECT * FROM users WHERE id=?", (user_id,))
     if not cursor.fetchone():
-        now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+        now = datetime.datetime.now(egypt).strftime("%Y-%m-%d %H:%M:%S")
 
         cursor.execute(
             "INSERT INTO users (id, balance, last_daily, invited_by, join_date) VALUES (?, 0, 0, ?, ?)",
@@ -27,17 +21,11 @@ def add_user(user_id, invited_by=None):
         )
         conn.commit()
 
-        if invited_by:
-            cursor.execute(
-                "UPDATE users SET balance = balance + 5 WHERE id=?",
-                (invited_by,)
-            )
-            conn.commit()
-
 
 def get_balance(user_id):
     cursor.execute("SELECT balance FROM users WHERE id=?", (user_id,))
-    return cursor.fetchone()[0]
+    result = cursor.fetchone()
+    return result[0] if result else 0
 
 
 def add_balance(user_id, amount):
@@ -50,7 +38,8 @@ def add_balance(user_id, amount):
 
 def get_last_daily(user_id):
     cursor.execute("SELECT last_daily FROM users WHERE id=?", (user_id,))
-    return cursor.fetchone()[0]
+    result = cursor.fetchone()
+    return result[0] if result else 0
 
 
 def update_daily(user_id, time):
@@ -63,4 +52,5 @@ def update_daily(user_id, time):
 
 def get_join_date(user_id):
     cursor.execute("SELECT join_date FROM users WHERE id=?", (user_id,))
-    return cursor.fetchone()[0]
+    result = cursor.fetchone()
+    return result[0] if result else "غير متوفر"
