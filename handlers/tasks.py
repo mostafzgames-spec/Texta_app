7a4b2b52247d7bff59d8169b071e0c1b29bd1014
mostafzgames-spec <mostafzgames@gmail.com>
2026-03-27 02:35:05 +0200

    Delete handlers/tasks.py

--- handlers/tasks.py
+++
@@ -1,5 +0,0 @@
-from telegram import Update
-from telegram.ext import ContextTypes
-
-async def tasks(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("📌 المهام سيتم إضافتها قريباً")
