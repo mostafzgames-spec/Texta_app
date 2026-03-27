5c04ec26143df7793f9dd514067a379b8b789bdf
mostafzgames-spec <mostafzgames@gmail.com>
2026-03-27 02:34:50 +0200

    Create tasks_data.py

---
+++ modules/tasks/tasks_data.py
@@ -0,0 +1,16 @@
+tasks_list = [
+    {
+        "id": 1,
+        "name": "زيارة موقع 1",
+        "desc": "ادخل الموقع وانتظر 20 ثانية",
+        "reward": 10,
+        "link": "https://example.com"
+    },
+    {
+        "id": 2,
+        "name": "زيارة موقع 2",
+        "desc": "افتح الرابط وخد نقاط",
+        "reward": 15,
+        "link": "https://google.com"
+    }
+]
