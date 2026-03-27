93076a9a4428bd2d2d53a9447eb6c057983631f8
mostafzgames-spec <mostafzgames@gmail.com>
2026-03-27 02:39:24 +0200

    Update init_db.py

--- init_db.py
+++ init_db.py
@@ -1,17 +1,35 @@
-# تنفيذ المهام (مرة يوميًا)
-cursor.execute("""
-CREATE TABLE IF NOT EXISTS task_done (
-    user_id INTEGER,
-    task_id INTEGER,
-    last_time INTEGER,
-    PRIMARY KEY (user_id, task_id)
-)
-""")
+from database import cursor, conn
 
-# فاصل بين المهام (3 دقائق)
-cursor.execute("""
-CREATE TABLE IF NOT EXISTS user_last_task (
-    user_id INTEGER PRIMARY KEY,
-    last_time INTEGER
-)
-""")
+def create_tables():
+
+    # جدول المستخدمين
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS users (
+        id INTEGER PRIMARY KEY,
+        balance INTEGER DEFAULT 0,
+        last_daily INTEGER DEFAULT 0,
+        invited_by INTEGER,
+        join_date TEXT
+    )
+    """)
+
+    # جدول تنفيذ المهام (مرة يوميًا)
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS task_done (
+        user_id INTEGER,
+        task_id INTEGER,
+        last_time INTEGER,
+        PRIMARY KEY (user_id, task_id)
+    )
+    """)
+
+    # جدول الفاصل بين المهام (3 دقائق)
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS user_last_task (
+        user_id INTEGER PRIMARY KEY,
+        last_time INTEGER
+    )
+    """)
+
+    conn.commit()
+    print("✅ Database Ready")
