d6335ddf101d571aca7100c862b19586e07a7614
mostafzgames-spec <mostafzgames@gmail.com>
2026-03-28 19:15:23 +0200

    Update main.py

--- user_bot/main.py
+++ user_bot/main.py
@@ -1,6 +1,12 @@
 import telebot
-from config import BOT_TOKEN
+import os
+import sys
+
+# حل مشكلة الاستيراد من خارج المجلد
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+
 from init_db import create_tables
+from config import BOT_TOKEN
 
 from modules.account.account import register_account
 from modules.tasks.tasks import register_tasks
@@ -8,8 +14,10 @@ from modules.referral.referral import register_referral
 
 bot = telebot.TeleBot(BOT_TOKEN)
 
+# إنشاء الجداول
 create_tables()
 
+# تسجيل الموديولز
 register_account(bot)
 register_tasks(bot)
 register_referral(bot)
