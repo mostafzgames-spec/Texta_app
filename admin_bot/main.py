0d2eea01de1ce5b6b80d2737974fdc086773f6e5
mostafzgames-spec <mostafzgames@gmail.com>
2026-03-28 20:12:25 +0200

    Update main.py

--- admin_bot/main.py
+++ admin_bot/main.py
@@ -6,15 +6,17 @@ import sys
 sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 
 from admin_bot.config import ADMIN_BOT_TOKEN
+
+# استدعاء الموديولز
 from modules.tasks.add_task import register_add_task
-from modules.broadcast.broadcast import register_broadcast
+from modules.broadcast import broadcast
 
 # إنشاء البوت
 bot = telebot.TeleBot(ADMIN_BOT_TOKEN)
 
-# تسجيل الموديولز
+# تسجيل الأنظمة
 register_add_task(bot)
-register_broadcast(bot)
+broadcast.register_broadcast(bot)
 
 print("Admin bot running...")
 
