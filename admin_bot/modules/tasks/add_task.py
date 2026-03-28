12049aff0985c543d646a00367bdcb0544914ed3
mostafzgames-spec <mostafzgames@gmail.com>
2026-03-28 19:20:58 +0200

    Update add_task.py

--- admin_bot/modules/tasks/add_task.py
+++ admin_bot/modules/tasks/add_task.py
@@ -1,50 +1,12 @@
-from database import get_connection
-from admin_bot.config import ADMIN_ID
+@bot.message_handler(commands=['start'])
+def start_admin(message):
+    if message.from_user.id != ADMIN_ID:
+        bot.send_message(message.chat.id, "❌ انت مش ادمن")
+        return
 
-data = {}
-
-def register_add_task(bot):
-
-    @bot.message_handler(commands=['addtask'])
-    def add(message):
-        if message.from_user.id != ADMIN_ID:
-            return
-
-        data[message.from_user.id] = {}
-        bot.send_message(message.chat.id, "اسم المهمة:")
-
-    @bot.message_handler(func=lambda m: m.from_user.id in data)
-    def steps(message):
-        uid = message.from_user.id
-
-        if "title" not in data[uid]:
-            data[uid]["title"] = message.text
-            bot.send_message(message.chat.id, "الوصف:")
-        elif "description" not in data[uid]:
-            data[uid]["description"] = message.text
-            bot.send_message(message.chat.id, "الرابط:")
-        elif "link" not in data[uid]:
-            data[uid]["link"] = message.text
-            bot.send_message(message.chat.id, "النقاط:")
-        else:
-            data[uid]["reward"] = int(message.text)
-
-            conn = get_connection()
-            cur = conn.cursor()
-
-            cur.execute("""
-            INSERT INTO tasks (title, description, link, reward)
-            VALUES (%s, %s, %s, %s)
-            """, (
-                data[uid]["title"],
-                data[uid]["description"],
-                data[uid]["link"],
-                data[uid]["reward"]
-            ))
-
-            conn.commit()
-            cur.close()
-            conn.close()
-
-            bot.send_message(message.chat.id, "✅ تمت الإضافة")
-            del data[uid]
+    bot.send_message(
+        message.chat.id,
+        "👑 مرحبا بك في بوت الادمن\n\n"
+        "📌 الأوامر:\n"
+        "/addtask - إضافة مهمة"
+    )
