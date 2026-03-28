51c8b2646313d5930aedb321ca8b9ecd903b5b5e
mostafzgames-spec <mostafzgames@gmail.com>
2026-03-28 19:05:34 +0200

    Update main.py

--- admin_bot/main.py
+++ admin_bot/main.py
@@ -1,1 +1,10 @@
+import telebot
+from config import BOT_TOKEN
+from modules.tasks.add_task import register_add_task
 
+bot = telebot.TeleBot(BOT_TOKEN)
+
+register_add_task(bot)
+
+print("Admin bot running...")
+bot.infinity_polling()
