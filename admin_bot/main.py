babc7d132afdedaa9d1ace4a5a0699b1869c39b1
mostafzgames-spec <mostafzgames@gmail.com>
2026-03-28 19:46:09 +0200

    Update main.py

--- admin_bot/main.py
+++ admin_bot/main.py
@@ -2,15 +2,21 @@ import telebot
 import os
 import sys
 
-# حل نفس المشكلة هنا
+# حل مشكلة الاستيراد بين المجلدات
 sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 
-from config import BOT_TOKEN
+from admin_bot.config import ADMIN_BOT_TOKEN
 from modules.tasks.add_task import register_add_task
+from modules.broadcast.broadcast import register_broadcast
 
-bot = telebot.TeleBot(BOT_TOKEN)
+# إنشاء البوت
+bot = telebot.TeleBot(ADMIN_BOT_TOKEN)
 
+# تسجيل الموديولز
 register_add_task(bot)
+register_broadcast(bot)
 
 print("Admin bot running...")
+
+# تشغيل البوت
 bot.infinity_polling()
