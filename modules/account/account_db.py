19ae5551031ecc8be259986b29b50edf611b67d7
mostafzgames-spec <mostafzgames@gmail.com>
2026-03-28 19:01:59 +0200

    Update account_db.py

--- modules/account/account_db.py
+++ modules/account/account_db.py
@@ -1,5 +1,24 @@
-from database import cursor
+from database import get_connection
 
-def get_user_data(user_id):
-    cursor.execute("SELECT id, join_date FROM users WHERE id=?", (user_id,))
-    return cursor.fetchone()
+def create_user(user_id):
+    conn = get_connection()
+    cur = conn.cursor()
+
+    cur.execute("SELECT * FROM users WHERE user_id=%s", (user_id,))
+    if not cur.fetchone():
+        cur.execute("INSERT INTO users (user_id) VALUES (%s)", (user_id,))
+        conn.commit()
+
+    cur.close()
+    conn.close()
+
+def get_user(user_id):
+    conn = get_connection()
+    cur = conn.cursor()
+
+    cur.execute("SELECT * FROM users WHERE user_id=%s", (user_id,))
+    user = cur.fetchone()
+
+    cur.close()
+    conn.close()
+    return user
