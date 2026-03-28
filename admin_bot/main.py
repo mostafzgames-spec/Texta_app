dc450177964a4975c81d1d1401e2987741554a74
mostafzgames-spec <mostafzgames@gmail.com>
2026-03-28 19:15:56 +0200

    Update main.py

--- admin_bot/main.py
+++ admin_bot/main.py
@@ -1,4 +1,10 @@
 import telebot
+import os
+import sys
+
+# حل نفس المشكلة هنا
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+
 from config import BOT_TOKEN
 from modules.tasks.add_task import register_add_task
 
