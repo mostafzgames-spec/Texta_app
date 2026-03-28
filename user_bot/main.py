67c9ad6112ffa9cf97a6ebb5ee33aaac24d3ef14
mostafzgames-spec <mostafzgames@gmail.com>
2026-03-28 19:01:18 +0200

    Update main.py

--- user_bot/main.py
+++ user_bot/main.py
@@ -1,1 +1,18 @@
+import telebot
+from config import BOT_TOKEN
+from init_db import create_tables
 
+from modules.account.account import register_account
+from modules.tasks.tasks import register_tasks
+from modules.referral.referral import register_referral
+
+bot = telebot.TeleBot(BOT_TOKEN)
+
+create_tables()
+
+register_account(bot)
+register_tasks(bot)
+register_referral(bot)
+
+print("User bot running...")
+bot.infinity_polling()
