5f210c80c041505bf3479843f10588e7def5a795
mostafzgames-spec <mostafzgames@gmail.com>
2026-03-29 19:25:46 +0200

    Update main.py

--- user_bot/main.py
+++ user_bot/main.py
@@ -1,22 +1,26 @@
 import telebot
+import os
+import sys
 from telebot.types import ReplyKeyboardMarkup, KeyboardButton
+
+# 🔥 الحل هنا
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 
 from modules.tasks.tasks import register_tasks
 
-BOT_TOKEN = "PUT_YOUR_TOKEN_HERE"
+BOT_TOKEN = os.getenv("USER_BOT_TOKEN")
 
 bot = telebot.TeleBot(BOT_TOKEN)
 
 # تسجيل نظام المهام
 register_tasks(bot)
 
-# 📌 قائمة رئيسية
+# قائمة رئيسية
 @bot.message_handler(commands=['start'])
 def start(message):
     markup = ReplyKeyboardMarkup(resize_keyboard=True)
 
     btn_tasks = KeyboardButton("المهام")
-
     markup.add(btn_tasks)
 
     bot.send_message(
