d0df016f79447272d1e79b9df0210ebd94c4cc20
mostafzgames-spec <mostafzgames@gmail.com>
2026-03-29 19:11:14 +0200

    Register broadcast system in admin bot

--- admin_bot/main.py
+++ admin_bot/main.py
@@ -2,16 +2,20 @@ import telebot
 import os
 import sys
 
-# حل الاستيراد
+# مهم عشان يقرأ modules اللي بره
 sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 
 from admin_bot.config import ADMIN_BOT_TOKEN
+
+# استدعاء الأنظمة
 from modules.tasks.add_task import register_add_task
+from modules.broadcast import register_broadcast
 
 bot = telebot.TeleBot(ADMIN_BOT_TOKEN)
 
-# تسجيل النظام
+# تشغيل الأنظمة
 register_add_task(bot)
+register_broadcast(bot)
 
 print("Admin bot running...")
 
