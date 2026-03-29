34b084a939767415ba255384d16531fa2fd928c6
mostafzgames-spec <mostafzgames@gmail.com>
2026-03-29 19:03:36 +0200

    Refactor comments and streamline bot initialization
    Refactor import comments and clean up code structure.

--- admin_bot/main.py
+++ admin_bot/main.py
@@ -2,23 +2,17 @@ import telebot
 import os
 import sys
 
-# حل مشكلة الاستيراد بين المجلدات
+# حل الاستيراد
 sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 
 from admin_bot.config import ADMIN_BOT_TOKEN
-
-# استدعاء الموديولز
 from modules.tasks.add_task import register_add_task
-from modules.broadcast import broadcast
 
-# إنشاء البوت
 bot = telebot.TeleBot(ADMIN_BOT_TOKEN)
 
-# تسجيل الأنظمة
+# تسجيل النظام
 register_add_task(bot)
-broadcast.register_broadcast(bot)
 
 print("Admin bot running...")
 
-# تشغيل البوت
 bot.infinity_polling()
