d8d939544c0ca78864e82920fab7320a64ffff1d
mostafzgames-spec <mostafzgames@gmail.com>
2026-03-31 20:21:26 +0200

    Update start.py

--- user_bot/handlers/start.py
+++ user_bot/handlers/start.py
@@ -1,27 +1,10 @@
 from aiogram import Router
 from aiogram.types import Message
-from aiogram.filters import CommandStart
-from sqlalchemy import select
-
-from shared.database.connection import SessionLocal
-from user_bot.db.user_model import User
 
 router = Router()
 
-@router.message(CommandStart())
+@router.message()
 async def start_handler(message: Message):
-    async with SessionLocal() as session:
-        result = await session.execute(
-            select(User).where(User.telegram_id == message.from_user.id)
-        )
-        user = result.scalar()
-
-        if not user:
-            new_user = User(telegram_id=message.from_user.id)
-            session.add(new_user)
-            await session.commit()
-            print("New user added:", message.from_user.id)
-        else:
-            print("User already exists:", message.from_user.id)
-
-    await message.answer("👋 أهلاً بيك في بوت Mafhumatk")
+    if message.text == "/start":
+        print("Start triggered")
+        await message.answer("👋 أهلاً بيك في بوت Mafhumatk")
