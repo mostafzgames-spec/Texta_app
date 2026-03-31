2f0a23f091aca354e7db12c201de2211009d5482
mostafzgames-spec <mostafzgames@gmail.com>
2026-03-31 18:47:45 +0200

    Update main.py

--- user_bot/main.py
+++ user_bot/main.py
@@ -1,25 +1,23 @@
 import asyncio
 from aiogram import Bot, Dispatcher
-from user_bot.config import BOT_TOKEN
-from user_bot.handlers.start import router
-from shared.database.connection import engine
-from user_bot.db.base import Base
+from aiogram.types import Message
+
+import os
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 async def main():
-    # إنشاء البوت
     bot = Bot(token=BOT_TOKEN)
     dp = Dispatcher()
 
-    # إضافة الراوتر
-    dp.include_router(router)
-
-    # إنشاء الجداول في قاعدة البيانات
-    async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
+    @dp.message()
+    async def echo(message: Message):
+        print("Message received:", message.text)
+        await message.answer("🔥 البوت شغال!")
 
     print("Bot is running...")
 
-    # تشغيل البوت
+    await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
