1dd582444aeb3c630e9e9f0f21b5a49937d55d12
mostafzgames-spec <mostafzgames@gmail.com>
2026-03-31 18:39:51 +0200

    Update main.py

--- user_bot/main.py
+++ user_bot/main.py
@@ -6,16 +6,20 @@ from shared.database.connection import engine
 from user_bot.db.base import Base
 
 async def main():
+    # إنشاء البوت
     bot = Bot(token=BOT_TOKEN)
     dp = Dispatcher()
 
+    # إضافة الراوتر
     dp.include_router(router)
 
-    # create tables
+    # إنشاء الجداول في قاعدة البيانات
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
 
     print("Bot is running...")
+
+    # تشغيل البوت
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
