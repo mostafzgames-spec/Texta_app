9808e414b5e13501cbb4f1b2e0961342f64b5600
mostafzgames-spec <mostafzgames@gmail.com>
2026-04-01 06:12:21 +0200

    Update connection.py

--- shared/database/connection.py
+++ shared/database/connection.py
@@ -1,15 +1,15 @@
-from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 import os
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-# fix asyncpg
-if DATABASE_URL and DATABASE_URL.startswith("postgresql://"):
-    DATABASE_URL = DATABASE_URL.replace(
-        "postgresql://",
-        "postgresql+asyncpg://"
-    )
+# Railway بيبعت postgres:// لازم نحولها لـ async
+if DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql+asyncpg://")
 
 engine = create_async_engine(DATABASE_URL, echo=True)
 
-SessionLocal = async_sessionmaker(engine, expire_on_commit=False)
+SessionLocal = async_sessionmaker(
+    engine,
+    expire_on_commit=False
+)
